edd5839e59f07421c25bd5be0c817c4bf1f39034
Luis Perichon <luisperichon@gmail.com>
2014-06-26 16:24:46 -0300

    revert

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
-var app = angular.module('myApp', ['onsen.directives']);
+(function(){
+	'use strict';
+	var app = angular.module('myApp', ['onsen.directives']);
+	app.controller("homeController", function($scope) {
 
-document.addEventListener('deviceready', function() {
-    angular.bootstrap(document, ['myApp']);
-}, false);
+	    $scope.callSchool = function(){       
+	        document.location = "tel:47333801";
+	    }
 
-app.controller("homeController", function($scope) {
+	});
 
-    $scope.callSchool = function(){       
-        document.location = "tel:47333801";
-    }
+	app.controller("contactController", function($scope) {
 
-});
+	    $scope.submit = function(recipient, subject, body){       
+	        window.plugins.email.open({
+			    to:      [recipient],
+			    subject: subject,
+			    body:    body
+			}); 
+	    }
 
-app.controller("contactController", function($scope) {
-
-    $scope.submit = function(recipient, subject, body){       
-        window.plugins.email.open({
-		    to:      [recipient],
-		    subject: subject,
-		    body:    body
-		}); 
-    }
-
-});
+	});
+})();
