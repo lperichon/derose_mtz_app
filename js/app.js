86e21e2545282b757d5b4e7711f3a4f5fe9f0fdb
Luis Perichon <luisperichon@gmail.com>
2014-06-28 18:49:05 -0300

    blog

--- js/app.js
+++ js/app.js
@@ -54,4 +54,71 @@
 	    	}
 	    }
 	});
+
+	app.controller('RecentPostsController', function($scope, $location, Blog) {
+		$scope.getRecent = function() {
+
+			$scope.msg = "Loading...";
+
+			Blog.getRecentPosts().then(function(data) {
+				$scope.data = data;
+
+				if ($scope.data.posts.length < 1)
+				{
+					$scope.msg = "Nothing found.";
+				}else{
+					$scope.msg = undefined;
+				}
+
+			});
+		};
+
+		// call service
+		$scope.getRecent();
+	});
+
+	app.controller('PostController', function($scope, $location, Blog, $sce, $routeParams) {
+		$scope.getPost = function() {
+
+			$scope.msg = "Loading...";
+
+			Blog.getPost($routeParams.id).then(function(data) {
+				$scope.post = data.post;
+
+				// make trusted html in 1.2
+				$scope.post.content = $sce.trustAsHtml(data.post.content);
+
+				if (typeof data.post == "undefined")
+				{
+					$scope.msg = "Nothing found.";
+				}else{
+					$scope.msg = undefined;
+				}
+			});
+		};
+
+		// call service
+		$scope.getPost();
+	});
+
+	app.factory('Blog', function($http) {
+	    var Blog = {
+	        endpoint: 'http://www.derosemartinez.com.ar/api',
+
+	        getRecentPosts: function() {
+	            return this.makeCall(this.endpoint + '/get_recent_posts/?callback=JSON_CALLBACK');
+	        },
+
+	        getPost: function(id) {
+	            return this.makeCall(this.endpoint + '/get_post/?id='+id+'&callback=JSON_CALLBACK');
+	        },
+
+	        makeCall: function(url){
+	            return $http.jsonp(url).then(function(response){
+	                return response.data;
+	            });            
+	        }
+	    };
+	    return Blog;
+	});
 })();
