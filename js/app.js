f0af1c3e316f0620d86353761c3b61e4ca877c6c
Luis Perichon <luisperichon@gmail.com>
2014-06-28 13:33:15 -0300

    facebook button

--- js/app.js
+++ js/app.js
@@ -32,7 +32,26 @@
 	    }
 	    
 	    $scope.fb = function(id){
-	    	
+	    	if (device.platform == 'iOS') {
+	    		appAvailability.check(
+				    'fb://', // URI Scheme
+				    function() {  // Success callback
+				        window.open('fb://profile/' + id, '_system', 'location=no');
+				    },
+				    function() {  // Error callback  
+				    }
+				);
+	    	}
+	    	else if(devise.platform == 'Android') {
+	    		appAvailability.check(
+				    'com.facebook.katana', // URI Scheme
+				    function() {           // Success callback
+				        window.open('fb://profile/' + id, '_system', 'location=no');
+				    },
+				    function() {           // Error callback
+				    }
+				);
+	    	}
 	    }
 	});
 })();
